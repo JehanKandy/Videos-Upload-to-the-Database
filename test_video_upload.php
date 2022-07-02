8b79054cdeb1e9e08845dcb0e4065602bd14828e
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-02 23:38:52 +0530

    Add files via upload

---
+++ test_video_upload.php
@@ -0,0 +1,35 @@
+<?php include_once("lib/function/function.php"); ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+<style>
+    .video-content{
+        padding-top: 80px;
+        padding-left: 150px;
+        padding-right: 150px;        
+    }
+</style>
+
+<div class="video-content">
+    Videos
+    <br><br><br>
+
+    <?php
+        if(isset($_POST['submit'])){
+            $result = video_upoload($_POST['username'], $_FILES['video']);
+            return $result;
+        }    
+        /* if are there any file type error the print
+            Wrong File Type */
+            
+        if(isset($_GET['error'])){
+            echo "<p>".$_GET['error']."</p>";
+        }
+    ?>
+
+
+    <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST" enctype="multipart/form-data">
+        Username : <input type="text" name="username">
+        Video : <input type="file" name="video" accept="video/*">
+        <input type="submit" value="Add" name="submit">
+    </form>
+</div>
---
+++ view.php
@@ -0,0 +1,22 @@
+<?php include_once("lib/function/function.php"); ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+<style>
+    .view-video{
+        padding-top: 80px;
+        padding-left: 150px;
+        padding-right: 150px;        
+    }
+    video{
+        width: 640px;
+        height: 360px;
+    }
+    a{
+        font-size: 20px;
+    }
+</style>
+
+<div class="view-video">
+    <a href="test_video_upload.php">Upload new Video</a><br><br><br>
+    <?php uploded_videos(); ?>
+</div>
