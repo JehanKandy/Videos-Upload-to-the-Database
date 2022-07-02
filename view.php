2a08470222835d04d7696767fd60682e9830b45c
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-02 23:39:51 +0530

    Update view.php

--- view.php
+++ view.php
@@ -1,4 +1,4 @@
-<?php include_once("lib/function/function.php"); ?>
+<?php include_once("function.php"); ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     
 <style>
