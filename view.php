8dae5ddd2454d1fa2214efdacddd0b1e48091f95
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-03 08:09:12 +0530

    Update view.php

--- view.php
+++ view.php
@@ -1,6 +1,7 @@
-<?php include_once("function.php"); ?>
+<?php include_once("lib/function/function.php"); ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+   
 <style>
     .view-video{
         padding-top: 80px;
@@ -8,11 +9,24 @@
         padding-right: 150px;        
     }
     video{
+        padding-left: 20px;
         width: 640px;
         height: 360px;
     }
     a{
         font-size: 20px;
+    }
+    .video-title{
+        background-color: rgb(233, 233, 233);
+        width: 620px;
+        margin-left: 20px;
+
+    }
+    .video-title-content{
+        padding-top: 10px;
+        padding-left: 25px;
+        padding-right: 25px;
+        padding-bottom: 10px;
     }
 </style>
 
